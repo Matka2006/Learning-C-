26f58ba8575852eb67095f0b3923db29c258ba55
Matka <tatmat2000@bk.ru>
2024-07-28 17:26:18 +0300

    LessonCycles

--- Program.cs
+++ Program.cs
@@ -1,79 +1,44 @@
-﻿class Program
+﻿
+
+namespace Homework
 {
-    static void Main(string[] args)
+    internal class Program
     {
+        static void Main(string[] args)
+        {
+            var last = 1;
+            for (int i = 0; i == 34; i += last)
+            {
+                Console.WriteLine(i);
+                last = i;
+                Console.Read();
+            }
 
-        if (!int.TryParse(Console.ReadLine(), out int a))
-        {
-            Console.WriteLine("Not a number!");
-            Console.Read();
-            return;
-        }
-        if (!int.TryParse(Console.ReadLine(), out int b))
-        {
-            Console.WriteLine("Not a number!");
-            Console.Read();
-            return;
-        }
+            for (int i = 0; i == 20; i += 2)
+            {
+                Console.WriteLine(i);
+                Console.Read();
+            }
 
-        var s = Console.ReadLine();
-        var boolVar = true;
-        if (s.Length == 0 || s.Length > 1 && !boolVar)
-        {
-            Console.WriteLine("Wrong sign");
-            Console.Read();
-            return;
-        }
+            int[] num = new[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            for (int i = 0; i == 5; i += 1)
+            {
+                foreach (int g in num)
+                {
+                    Console.WriteLine(g * i);
+                    Console.Read();
+                }
+            }
 
-        switch (s[0])
-        {
-            case '+':
-                Console.WriteLine("Result of {0} + {1} = {2}, 0b: {3}, 0x: {4} ", a, b, a + b, Convert.ToString(a + b, 2), Convert.ToString(a + b, 16));
-                break;
-            case '-':
-                Console.WriteLine("Result of {0} - {1} = {2}, 0b: {3}, 0x: {4} ", a, b, a - b, Convert.ToString(a - b, 2), Convert.ToString(a - b, 16));
-                break;
-            case '/':
-                Console.WriteLine("Result of {0} / {1} = {2}, 0b: {3}, 0x: {4} ", a, b, a / b, Convert.ToString(a / b, 2), Convert.ToString(a / b, 16));
-                break;
-            case '*':
-                Console.WriteLine("Result of {0} * {1} = {2}, 0b: {3}, 0x: {4} ", a, b, a * b, Convert.ToString(a * b, 2), Convert.ToString(a * b, 16));
-                break;
-            case '%':
-                Console.WriteLine("Result of {0} % {1} = {2}, 0b: {3}, 0x: {4} ", a, b, a % b, Convert.ToString(a % b, 2), Convert.ToString(a % b, 16));
-                break;
+            string password;
+            do
+            {
+                password = Console.ReadLine();
+            } while (password != "qwerty");
+
+
 
         }
-
-        Console.WriteLine("Also suggest using the following functions: &, |, ^");
-        var c = Console.ReadLine();
-        //var boolC = true;
-        if (c[0] != '&' && c[0] != '|' && c[0] != '^' && c[0] != 'N')
-        {
-            Console.WriteLine("Wrong sign");
-            Console.Read();
-            return;
-        }
-
-        switch (c[0])
-        {
-            case '&':
-                Console.WriteLine("Result of {0} & {1} = {2}, 0b: {3}, 0x: {4} ", a, b, a & b, Convert.ToString(a & b, 2), Convert.ToString(a & b, 16));
-                break;
-            case '|':
-                Console.WriteLine("Result of {0} | {1} = {2}, 0b: {3}, 0x: {4} ", a, b, a | b, Convert.ToString(a | b, 2), Convert.ToString(a | b, 16));
-                break;
-            case '^':
-                Console.WriteLine("Result of {0} ^ {1} = {2}, 0b: {3}, 0x: {4} ", a, b, a ^ b, Convert.ToString(a ^ b, 2), Convert.ToString(a ^ b, 16));
-                break;
-            case 'N':
-                break;
-
-        }
-
-        Console.WriteLine("Thank you for using my calculator!!!");
-        Console.Read();
-
     }
 }
 
